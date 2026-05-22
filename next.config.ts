353c08fd7b8f148f3113f440982398acde1946bd
PhanTriVietHoang <phantriviethoang@gmail.com>
2026-05-22 23:33:29 +0700

    amend

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    devIndicators: false,
     images: {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "pixabay.com",
+                hostname: "cdn.pixabay.com",
             },
         ],
     },
